170ce23c0953bd5c164400aa4643eb7fb9d69392
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2025-08-13 15:36:31 +0200

    creatinga custom for wach method

---
+++ myForEach.js
@@ -0,0 +1,14 @@
+//Working on creating a custom forEach method
+
+const myForEach = (array,callback) =>{
+        for(let i = 0;i<array.length;i++){
+            callback(array[i],i);
+        }    
+}
+
+function callback(element,index){
+   console.log(`Element ${element} is one this ${index} index`)
+}
+
+
+myForEach([1,2,3,4,5,6,7,8,9,8],callback);
