525e490c74c19c4a85bff44ce3f1f6bae6a0b012
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2025-08-18 16:54:20 +0200

    adding my manual reduce method

---
+++ myReduce.js
@@ -0,0 +1,17 @@
+//working on creating my custom myReduce
+Array.prototype.myReduce= function(callback){
+    let acc = this[0];
+    for(i = 1;i<this.length;i++){
+        acc = callback(acc,this[i]);
+    }
+    return acc;
+}
+
+function callback(a,b){
+    return a + b;
+}
+
+let myArr = [1,2,3,4,5,6,7,8,9];
+console.log(myArr.myReduce(callback))
+
+// myArr.reduce(())
